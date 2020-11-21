9ab4dd3545b1522f756332f76502fc9c359589d3
firdaus975 <choirulahamad31@gmail.com>
2020-11-21 19:57:11 +0700

    Update navbar.blade.php

--- resources/views/master/navbar.blade.php
+++ resources/views/master/navbar.blade.php
@@ -6,14 +6,14 @@
         </ul>
       </form>
       <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
-        <img alt="image" src="../assets/img/avatar/avatar-1.png" class="rounded-circle mr-1">
+        <img alt="image" src="{{asset('assets/img/avatar/avatar-1.png')}}" class="rounded-circle mr-1">
         <div class="d-sm-none d-lg-inline-block">{{Auth::user()->name}}<span class="caret"></span></div></a>
         <div class="dropdown-menu dropdown-menu-right">
         <a href="/profile/{{Auth::user()->id}}" class="dropdown-item has-icon">
             <i class="far fa-user"></i> Profile
           </a>
           <div class="dropdown-divider"></div>
-          <a href="/logout" class="dropdown-item has-icon text-danger">
+          <a href="{{Route('logout')}}" class="dropdown-item has-icon text-danger">
             <i class="fas fa-sign-out-alt"></i> Logout
           </a>
         </div>
