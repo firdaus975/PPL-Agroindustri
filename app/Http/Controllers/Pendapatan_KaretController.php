8404b80d4a583718b59b9cb85747dee83ce8c59e
firdaus975 <choirulahamad31@gmail.com>
2020-11-22 18:22:01 +0700

    Update Pendapatan_KaretController.php

--- app/Http/Controllers/Pendapatan_KaretController.php
+++ app/Http/Controllers/Pendapatan_KaretController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class Pendapatan_KaretController extends Controller
 {
     public function index()
     {
-        return view('pendapatan_karet.pendapatan_karet');
+        $data_pendapatan = \App\Pendapatan :: get();
+
+        return view('pendapatan_Karet.pendapatan',['data_pendapatan'=>$data_pendapatan]);
     }
 }
