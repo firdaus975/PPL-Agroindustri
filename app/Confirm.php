12537acef1d746ce71520bc3a0781983e58c7655
firdaus975 <choirulahamad31@gmail.com>
2020-11-23 07:18:39 +0700

    Update Confirm.php

--- app/Confirm.php
+++ app/Confirm.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Confirm extends Model
 {
-    //
+    protected $table ='confirms';
+    protected $fillable = ['name_confirms'];
+
+    public function pendapatan()
+    {
+        return $this->hasMany('App\Pendapatan');
+    }
 }
+
