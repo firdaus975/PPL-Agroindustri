53ed41ce8a2d24b5b1ac1e2f71ba622345d61c7e
firdaus975 <choirulahamad31@gmail.com>
2020-11-28 21:28:18 +0700

    Update pendapatan.blade.php

--- resources/views/pendapatan_karet/pendapatan.blade.php
+++ resources/views/pendapatan_karet/pendapatan.blade.php
@@ -7,42 +7,6 @@ Pendapatan
 @stop
 
 @section('content')
-<!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-  Launch demo modal
-</button>
-
-<!-- Modal -->
-<div class="modal fade" id="confirmasi" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <div class="row">
-          <div class="form-group col-md-7 col-12">
-            <select  name='confirm' class="custom-select"  >
-              <option selected>Status Konfirmasi</option>
-              <option  value="1"selected >Belum Dikonfirmasi</option>
-              <option  value="2"selected >Sudah Dikonfirmasi</option>
-            </select>
-            <div class="invalid-feedback">
-              Please fill in the 
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
 <section class="section">
     <div class="section-body">
       <div class="row mt-sm-4">
@@ -83,7 +47,9 @@ Pendapatan
                 <td><a href="{{Route('editPendapatan',[$pendapatan->id])}}"><button  class="btn btn-primary">Edit</button></a></td>
                 @endif
                 @if(Auth::user()->role_id == 2 )
-                <td><button  class="btn btn-primary" data-toggle="modal" data-target="#confirmasi">Konfirmasi</button></td>
+                <td><button type="button" class="btn btn-success float-right mb-1" data-toggle="modal" data-target="#modalpendapatanKaret{{$pendapatan->id}}">Konfirmasi</button>
+  </td>
+                
                 @endif
                 </tr>
                 @endforeach
@@ -100,5 +66,56 @@ Pendapatan
     </div>
     
   </section>
+  @foreach ($data_pendapatan as $data)
+            
+  !-- Modal -->
+<div class="modal fade" id="modalpendapatanKaret{{$data->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal-dialog">
+<div class="modal-content">
+<div class="modal-header">
+  <h5 class="modal-title">Update Barang</h5>
+  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+  <span aria-hidden="true">&times;</span>
+  </button>
+  </div>
+  <div class="modal-body">
+  <!--FORM UPDATE BARANG-->
+  <form method="post" action="{{Route('konfirmasi',[$data->id])}}" class="needs-validation" novalidate="">
+    {{ csrf_field() }}
+              <div class="card-body">
+                  <div class="row">
+                    <div class="form-group col-md ">
+                      <label>Berat Bersih</label>
+                      <input id="beratBersih" name ="beratBersih"type="number" class="form-control" value="{{$data->berat_bersih}}" required="" readonly>
+                      <div class="invalid-feedback">
+                        Please fill in the first name
+                      </div>
+                    </div>
+                    <div class="form-group">
+                      <input id="userId" name ='userId'type="text" class="form-control" value="{{$data->user_id}}" required="" hidden>
+                      <div class="invalid-feedback">
+                        Please fill in the first name
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="form-group col-md ">
+                      <label>Berat Kotor</label>
+                      <input id="beratKotor" type="number" name = 'beratKotor' class="form-control" value="{{$data->berat_kotor}}" required="" readonly>
+                      <div class="invalid-feedback">
+                        Please fill in the 
+                      </div>
+                    </div>
+                  </div>
+              </div>
+              <div class="modal-footer ">
+                <button class="btn btn-primary">Konfirmasi</button>
+              </div>
+            </form>
+</div>
+</div>
+</div>
+</div>
     
+@endforeach
 @endsection
