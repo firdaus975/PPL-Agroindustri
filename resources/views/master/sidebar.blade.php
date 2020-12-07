5381ebd7475f4800b4387bb617734a5feac7741f
firdaus975 <choirulahamad31@gmail.com>
2020-12-07 11:56:31 +0700

    Update sidebar.blade.php

--- resources/views/master/sidebar.blade.php
+++ resources/views/master/sidebar.blade.php
@@ -13,13 +13,13 @@
           <li class="nav-item dropdown">
             <a href="{{Route('pendapatan')}}" class="nav-link" ><i class="fas fa-columns"></i> <span>Pendapatan Karet</span></a>
           </li>
-          <li class=""><a class="nav-link" href="{{Route('perkiraan')}}"><i class="far fa-square"></i> <span>Perkiraan Pendapatan</span></a></li>
+          <li class=""><a class="nav-link" href="{{Route('perkiraan')}}"><i class="fas fa-chart-bar"></i> <span>Perkiraan Pendapatan</span></a></li>
           <li class="nav-item dropdown">
-            <a href="{{Route('keuangan')}}" class="nav-link"><i class="fas fa-th"></i> <span>Keuangan</span></a>
+            <a href="{{Route('keuangan')}}" class="nav-link"><i class="fas fa-money-check-alt"></i> <span>Keuangan</span></a>
           </li>
           @if(Auth::user()->role_id != 3)
           <li class="nav-item dropdown">
-            <a href="{{Route('pegawai')}}" class="nav-link"><i class="fas fa-th"></i> <span>Data Pegawai</span></a>
+            <a href="{{Route('pegawai')}}" class="nav-link"><i class="fas fa-users"></i> <span>Data Pegawai</span></a>
           </li>
           @endif
         </ul>
