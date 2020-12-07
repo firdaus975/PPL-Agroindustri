95e6f9dafa25ec158b0db16f3ae8b8dca1a1e7ea
firdaus975 <choirulahamad31@gmail.com>
2020-12-07 09:08:32 +0700

    Update pendapatan.blade.php

--- resources/views/pendapatan_karet/pendapatan.blade.php
+++ resources/views/pendapatan_karet/pendapatan.blade.php
@@ -10,9 +10,9 @@ Pendapatan
 
 <section class="section ">
     <form action="{{Route('cari')}}" method="GET" >
-      <div class="row ">
-          <select class="custom-select form-group col-md-2" name="bulan">
-            <option selected value="">Bulan</option>
+      <div class="row">
+          <select class="custom-select  col-md-2" name="bulan">
+            <option selected value="" disabled>Bulan</option>
             <option value="1">Januari</option>
             <option value="2">Februari</option>
             <option value="3">Maret</option>
@@ -26,8 +26,8 @@ Pendapatan
             <option value="11">November</option>
             <option value="12">Desember</option>
           </select>
-          <input type="number" name="tahun" value="{{old('cari2')}}" class="form-group" placeholder="Tahun">
-      <button type="submit" class="form-group">Cari</button>
+          <input type="number" name="tahun" style="margin-left: 5px"  class="form-control col-md-2" placeholder="Tahun">
+      <button type="submit" class="btn btn-success " style="margin-left: 10px">Cari</button>
       </div>
     </form>
     <div class="section-body">
@@ -56,26 +56,63 @@ Pendapatan
                 </tr>
               </thead>
               <tbody>
-                @foreach ($data_pendapatan as $pendapatan)
+                @php
+                    $i =1;
+                    $total = 0;
+                    $total2=0
+                @endphp
+                @forelse ($data_pendapatan as $pendapatan)
                 
                 <tr>
-                  <th scope="row">{{$pendapatan->id}}</th>
+                  <th scope="row">{{$i}}</th>
                   <td>{{$pendapatan->user->name}}</td>
                   <td>{{$pendapatan->berat_bersih}}</td>
                   <td>{{$pendapatan->berat_kotor}}</td>
-                <td>{{$pendapatan->created_at->format('Y/m/d')}}</td>
+                <td>{{$pendapatan->tanggal->format('Y/m/d')}}</td>
                 <td>{{$pendapatan->updated_at->format('Y/m/d')}}</td>
                 <td>{{$pendapatan->confirm->name_confirm}}</td>
                 @if(Auth::user()->role_id != 1 )
                 <td><a href="{{Route('editPendapatan',[$pendapatan->id])}}"><button  class="btn btn-primary">Edit</button></a></td>
                 @endif
-                @if(Auth::user()->role_id == 2 )
-                <td><button type="button" class="btn btn-success float-right mb-1" data-toggle="modal" data-target="#modalpendapatanKaret{{$pendapatan->id}}">Konfirmasi</button>
-  </td>
-                
+                @if(Auth::user()->role_id == 2 && $pendapatan->confirm_id == 1  )
+                <td><button type="button" class="btn btn-success float-right mb-1" data-toggle="modal" data-target="#modalpendapatanKaret{{$pendapatan->id}}">Konfirmasi</button></td>
+                @elseif(Auth::user()->role_id == 2 && $pendapatan->confirm_id == 2)
+                <td><button type="button" disabled class="btn btn-success float-right mb-1" data-toggle="modal" data-target="#modalpendapatanKaret{{$pendapatan->id}}">Konfirmasi</button></td>
                 @endif
                 </tr>
-                @endforeach
+                @php
+                 $i++;
+                   $total_kotor=$pendapatan->berat_kotor+$total;
+                   $total=$total+$pendapatan->berat_kotor; 
+                   $total_bersih=$pendapatan->berat_bersih+$total2;
+                   $total2=$total2+$pendapatan->berat_bersih; 
+                    
+                @endphp
+                @empty
+                <h1>Data Tidak Ditemukan</h1>
+                @endforelse
+                @if (empty($total_kotor))
+        
+        
+                @elseif($total_kotor!=0)
+                <table class="table table-striped"  width ="50px" >
+                  <strong>
+
+                    <th scope ="col" >Total  Berat Kotor</th>
+                    <th scope="col" style="text-align: right">{{$total_kotor}}</th>
+                  </strong>
+                </table>
+                <table class="table table-striped"  width ="50px" >
+                  <strong>
+
+                    <th scope ="col" >Total  Berat Bersih</th>
+                    <th scope="col" style="text-align: right">{{$total_bersih}}</th>
+                  </strong>
+                </table>
+                
+                @endif
+
+                
               </tbody>
             </table>
             @if(Auth::user()->role_id == 3 )
@@ -87,6 +124,7 @@ Pendapatan
         </div>
       </div>
     </div>
+
     
     {{$data_pendapatan->links()}}
   </section>
@@ -97,7 +135,7 @@ Pendapatan
 <div class="modal-dialog">
 <div class="modal-content">
 <div class="modal-header">
-  <h5 class="modal-title">Update Barang</h5>
+  <h5 class="modal-title">Konfirmasi Data Pendapatan</h5>
   <button type="button" class="close" data-dismiss="modal" aria-label="Close">
   <span aria-hidden="true">&times;</span>
   </button>
@@ -107,6 +145,15 @@ Pendapatan
   <form method="post" action="{{Route('konfirmasi',[$data->id])}}" class="needs-validation" novalidate="">
     {{ csrf_field() }}
               <div class="card-body">
+                <div class="row">
+                  <div class="form-group col-md ">
+                    <label>Tanggal</label>
+                    <input id="beratKotor" type="date" name = 'beratKotor' class="form-control" value="{{$data->tanggal}}" required="" readonly>
+                    <div class="invalid-feedback">
+                      Please fill in the 
+                    </div>
+                  </div>
+                </div>
                   <div class="row">
                     <div class="form-group col-md ">
                       <label>Berat Bersih</label>
