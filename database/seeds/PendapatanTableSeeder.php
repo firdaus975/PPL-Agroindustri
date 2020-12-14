7bdca7c4290ba58da9cd839112ad849002d62542
firdaus975 <choirulahamad31@gmail.com>
2020-12-14 22:58:50 +0700

    Update PendapatanTableSeeder.php

--- database/seeds/PendapatanTableSeeder.php
+++ database/seeds/PendapatanTableSeeder.php
@@ -16,9 +16,9 @@ class PendapatanTableSeeder extends Seeder
             'confirm_id' => 2,
             'berat_bersih'=> 60,
             'berat_kotor'=>69,
-            'tanggal'=>'2020-10-01',
-            'created_at'=>'2020-10-01 00:00:00',
-            'updated_at'=>'2020-10-01 00:00:00', 
+            'tanggal'=>'2020-12-01',
+            'created_at'=>'2020-12-01 00:00:00',
+            'updated_at'=>'2020-12-01 00:00:00', 
             'user_id' =>4,
         ]);
 
@@ -27,9 +27,9 @@ class PendapatanTableSeeder extends Seeder
             'confirm_id' => 2,
             'berat_bersih'=> 60,
             'berat_kotor'=>77,
-            'tanggal'=>'2020-10-02',
-            'created_at'=>'2020-10-02 00:00:00',
-            'updated_at'=>'2020-10-02 00:00:00', 
+            'tanggal'=>'2020-12-02',
+            'created_at'=>'2020-12-02 00:00:00',
+            'updated_at'=>'2020-12-02 00:00:00', 
             'user_id' =>4,
         ]);
         DB::table('pendapatans')->insert
@@ -37,9 +37,9 @@ class PendapatanTableSeeder extends Seeder
             'confirm_id' => 2,
             'berat_bersih'=> 67,
             'berat_kotor'=>77,
-            'tanggal'=>'2020-10-03',
-            'created_at'=>'2020-10-03 00:00:00',
-            'updated_at'=>'2020-10-03 00:00:00', 
+            'tanggal'=>'2020-12-03',
+            'created_at'=>'2020-12-03 00:00:00',
+            'updated_at'=>'2020-12-03 00:00:00', 
             'user_id' =>4,
         ]);
         DB::table('pendapatans')->insert
@@ -47,9 +47,9 @@ class PendapatanTableSeeder extends Seeder
             'confirm_id' => 2,
             'berat_bersih'=>70,
             'berat_kotor'=>80,
-            'tanggal'=>'2020-10-04',
-            'created_at'=>'2020-10-04 00:00:00',
-            'updated_at'=>'2020-10-04 00:00:00', 
+            'tanggal'=>'2020-12-04',
+            'created_at'=>'2020-12-04 00:00:00',
+            'updated_at'=>'2020-12-04 00:00:00', 
             'user_id' =>4,
         ]);
         DB::table('pendapatans')->insert
@@ -57,9 +57,9 @@ class PendapatanTableSeeder extends Seeder
             'confirm_id' => 2,
             'berat_bersih'=> 66,
             'berat_kotor'=>77,
-            'tanggal'=>'2020-10-05',
-            'created_at'=>'2020-10-05 00:00:00',
-            'updated_at'=>'2020-10-05 00:00:00', 
+            'tanggal'=>'2020-12-05',
+            'created_at'=>'2020-12-05 00:00:00',
+            'updated_at'=>'2020-12-05 00:00:00', 
             'user_id' =>4,
         ]);
         DB::table('pendapatans')->insert
@@ -67,9 +67,9 @@ class PendapatanTableSeeder extends Seeder
             'confirm_id' => 2,
             'berat_bersih'=> 60,
             'berat_kotor'=>75,
-            'tanggal'=>'2020-10-06',
-            'created_at'=>'2020-10-06 00:00:00',
-            'updated_at'=>'2020-10-06 00:00:00', 
+            'tanggal'=>'2020-12-06',
+            'created_at'=>'2020-12-06 00:00:00',
+            'updated_at'=>'2020-12-06 00:00:00', 
             'user_id' =>4,
         ]);
         DB::table('pendapatans')->insert
@@ -77,9 +77,9 @@ class PendapatanTableSeeder extends Seeder
             'confirm_id' => 2,
             'berat_bersih'=> 62,
             'berat_kotor'=>75,
-            'tanggal'=>'2020-10-07',
-            'created_at'=>'2020-10-07 00:00:00',
-            'updated_at'=>'2020-10-07 00:00:00', 
+            'tanggal'=>'2020-12-07',
+            'created_at'=>'2020-12-07 00:00:00',
+            'updated_at'=>'2020-12-07 00:00:00', 
             'user_id' =>4,
         ]);
         DB::table('pendapatans')->insert
@@ -87,9 +87,9 @@ class PendapatanTableSeeder extends Seeder
             'confirm_id' => 2,
             'berat_bersih'=> 56,
             'berat_kotor'=>70,
-            'tanggal'=>'2020-10-08',
-            'created_at'=>'2020-10-08 00:00:00',
-            'updated_at'=>'2020-10-08 00:00:00', 
+            'tanggal'=>'2020-12-08',
+            'created_at'=>'2020-12-08 00:00:00',
+            'updated_at'=>'2020-12-08 00:00:00', 
             'user_id' =>4,
         ]);
         DB::table('pendapatans')->insert
@@ -97,9 +97,9 @@ class PendapatanTableSeeder extends Seeder
             'confirm_id' => 2,
             'berat_bersih'=> 68,
             'berat_kotor'=>80,
-            'tanggal'=>'2020-10-09',
-            'created_at'=>'2020-10-09 00:00:00',
-            'updated_at'=>'2020-10-09 00:00:00', 
+            'tanggal'=>'2020-12-09',
+            'created_at'=>'2020-12-09 00:00:00',
+            'updated_at'=>'2020-12-09 00:00:00', 
             'user_id' =>4,
         ]);
         DB::table('pendapatans')->insert
@@ -107,9 +107,9 @@ class PendapatanTableSeeder extends Seeder
             'confirm_id' => 2,
             'berat_bersih'=> 76,
             'berat_kotor'=>87,
-            'tanggal'=>'2020-10-10',
-            'created_at'=>'2020-10-10 00:00:00',
-            'updated_at'=>'2020-10-10 00:00:00', 
+            'tanggal'=>'2020-12-10',
+            'created_at'=>'2020-12-10 00:00:00',
+            'updated_at'=>'2020-12-10 00:00:00', 
             'user_id' =>4,
         ]);
         DB::table('pendapatans')->insert
@@ -117,212 +117,12 @@ class PendapatanTableSeeder extends Seeder
             'confirm_id' => 2,
             'berat_bersih'=> 56,
             'berat_kotor'=>65,
-            'tanggal'=>'2020-10-11',
-            'created_at'=>'2020-10-11 00:00:00',
-            'updated_at'=>'2020-10-11 00:00:00', 
+            'tanggal'=>'2020-12-11',
+            'created_at'=>'2020-1-11 00:00:00',
+            'updated_at'=>'2020-12-11 00:00:00', 
             'user_id' =>4,
         ]);
-        DB::table('pendapatans')->insert
-        ([
-            'confirm_id' => 2,
-            'berat_bersih'=> 70,
-            'berat_kotor'=>85,
-            'tanggal'=>'2020-10-12',
-            'created_at'=>'2020-10-12 00:00:00',
-            'updated_at'=>'2020-10-12 00:00:00', 
-            'user_id' =>4,
-        ]);
-        DB::table('pendapatans')->insert
-        ([
-            'confirm_id' => 2,
-            'berat_bersih'=> 56,
-            'berat_kotor'=>65,
-            'tanggal'=>'2020-10-13',
-            'created_at'=>'2020-10-13 00:00:00',
-            'updated_at'=>'2020-10-13 00:00:00', 
-            'user_id' =>4,
-        ]);
-        DB::table('pendapatans')->insert
-        ([
-            'confirm_id' => 2,
-            'berat_bersih'=> 66,
-            'berat_kotor'=>75,
-            'tanggal'=>'2020-10-14',
-            'created_at'=>'2020-10-14 00:00:00',
-            'updated_at'=>'2020-10-14 00:00:00', 
-            'user_id' =>4,
-        ]);
-        DB::table('pendapatans')->insert
-        ([
-            'confirm_id' => 2,
-            'berat_bersih'=> 58,
-            'berat_kotor'=>65,
-            'tanggal'=>'2020-10-15',
-            'created_at'=>'2020-10-15 00:00:00',
-            'updated_at'=>'2020-10-15 00:00:00', 
-            'user_id' =>4,
-        ]);
-        DB::table('pendapatans')->insert
-        ([
-            'confirm_id' => 2,
-            'berat_bersih'=> 56,
-            'berat_kotor'=>65,
-            'tanggal'=>'2020-10-16',
-            'created_at'=>'2020-10-16 00:00:00',
-            'updated_at'=>'2020-10-16 00:00:00', 
-            'user_id' =>4,
-        ]);
-        DB::table('pendapatans')->insert
-        ([
-            'confirm_id' => 2,
-            'berat_bersih'=> 50,
-            'berat_kotor'=>65,
-            'tanggal'=>'2020-10-17',
-            'created_at'=>'2020-10-17 00:00:00',
-            'updated_at'=>'2020-10-17 00:00:00', 
-            'user_id' =>4,
-        ]);
-        DB::table('pendapatans')->insert
-        ([
-            'confirm_id' => 2,
-            'berat_bersih'=> 58,
-            'berat_kotor'=>65,
-            'tanggal'=>'2020-10-18',
-            'created_at'=>'2020-10-18 00:00:00',
-            'updated_at'=>'2020-10-18 00:00:00', 
-            'user_id' =>4,
-        ]);
-        DB::table('pendapatans')->insert
-        ([
-            'confirm_id' => 2,
-            'berat_bersih'=> 56,
-            'berat_kotor'=>65,
-            'tanggal'=>'2020-10-19',
-            'created_at'=>'2020-10-19 00:00:00',
-            'updated_at'=>'2020-10-19 00:00:00', 
-            'user_id' =>4,
-        ]);
-        DB::table('pendapatans')->insert
-        ([
-            'confirm_id' => 2,
-            'berat_bersih'=> 58,
-            'berat_kotor'=>65,
-            'tanggal'=>'2020-10-20',
-            'created_at'=>'2020-10-20 00:00:00',
-            'updated_at'=>'2020-10-20 00:00:00', 
-            'user_id' =>4,
-        ]);
-        DB::table('pendapatans')->insert
-        ([
-            'confirm_id' => 2,
-            'berat_bersih'=> 76,
-            'berat_kotor'=>85,
-            'tanggal'=>'2020-10-21',
-            'created_at'=>'2020-10-21 00:00:00',
-            'updated_at'=>'2020-10-21 00:00:00', 
-            'user_id' =>4,
-        ]);
-        DB::table('pendapatans')->insert
-        ([
-            'confirm_id' => 2,
-            'berat_bersih'=> 65,
-            'berat_kotor'=>70,
-            'tanggal'=>'2020-10-22',
-            'created_at'=>'2020-10-22 00:00:00',
-            'updated_at'=>'2020-10-22 00:00:00', 
-            'user_id' =>4,
-        ]);
-        DB::table('pendapatans')->insert
-        ([
-            'confirm_id' => 2,
-            'berat_bersih'=> 68,
-            'berat_kotor'=>75,
-            'tanggal'=>'2020-10-23',
-            'created_at'=>'2020-10-23 00:00:00',
-            'updated_at'=>'2020-10-23 00:00:00', 
-            'user_id' =>4,
-        ]);
-        DB::table('pendapatans')->insert
-        ([
-            'confirm_id' => 2,
-            'berat_bersih'=> 69,
-            'berat_kotor'=>75,
-            'tanggal'=>'2020-10-24',
-            'created_at'=>'2020-10-24 00:00:00',
-            'updated_at'=>'2020-10-24 00:00:00', 
-            'user_id' =>4,
-        ]);
-        DB::table('pendapatans')->insert
-        ([
-            'confirm_id' => 2,
-            'berat_bersih'=> 72,
-            'berat_kotor'=>80,
-            'tanggal'=>'2020-10-25',
-            'created_at'=>'2020-10-25 00:00:00',
-            'updated_at'=>'2020-10-25 00:00:00', 
-            'user_id' =>4,
-        ]);
-        DB::table('pendapatans')->insert
-        ([
-            'confirm_id' => 2,
-            'berat_bersih'=> 60,
-            'berat_kotor'=>70,
-            'tanggal'=>'2020-10-26',
-            'created_at'=>'2020-10-26 00:00:00',
-            'updated_at'=>'2020-10-26 00:00:00', 
-            'user_id' =>4,
-        ]);
-        DB::table('pendapatans')->insert
-        ([
-            'confirm_id' => 2,
-            'berat_bersih'=> 65,
-            'berat_kotor'=>70,
-            'tanggal'=>'2020-10-27',
-            'created_at'=>'2020-10-27 00:00:00',
-            'updated_at'=>'2020-10-27 00:00:00', 
-            'user_id' =>4,
-        ]);
-        DB::table('pendapatans')->insert
-        ([
-            'confirm_id' => 2,
-            'berat_bersih'=> 74,
-            'berat_kotor'=>85,
-            'tanggal'=>'2020-10-28',
-            'created_at'=>'2020-10-28 00:00:00',
-            'updated_at'=>'2020-10-28 00:00:00', 
-            'user_id' =>4,
-        ]);
-        DB::table('pendapatans')->insert
-        ([
-            'confirm_id' => 2,
-            'berat_bersih'=> 64,
-            'berat_kotor'=>75,
-            'tanggal'=>'2020-10-29',
-            'created_at'=>'2020-10-29 00:00:00',
-            'updated_at'=>'2020-10-29 00:00:00', 
-            'user_id' =>4,
-        ]);
-        DB::table('pendapatans')->insert
-        ([
-            'confirm_id' => 2,
-            'berat_bersih'=> 60,
-            'berat_kotor'=>75,
-            'tanggal'=>'2020-10-30',
-            'created_at'=>'2020-10-30 00:00:00',
-            'updated_at'=>'2020-10-30 00:00:00', 
-            'user_id' =>4,
-        ]);
-        DB::table('pendapatans')->insert
-        ([
-            'confirm_id' => 2,
-            'berat_bersih'=> 60,
-            'berat_kotor'=>76,
-            'tanggal'=>'2020-10-31',
-            'created_at'=>'2020-10-31 00:00:00',
-            'updated_at'=>'2020-10-31 00:00:00', 
-            'user_id' =>4,
-        ]);
-        
+
 
     }
 }
