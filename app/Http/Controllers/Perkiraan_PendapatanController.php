1a45c2d3c3e62859a1ee838f1c681a62d972ad42
firdaus975 <choirulahamad31@gmail.com>
2020-12-14 12:00:12 +0700

    Update Perkiraan_PendapatanController.php

--- app/Http/Controllers/Perkiraan_PendapatanController.php
+++ app/Http/Controllers/Perkiraan_PendapatanController.php
@@ -8,6 +8,30 @@ class Perkiraan_PendapatanController extends Controller
 {
     public function index()
     {
-        return view('perkiraan_pendapatan.perkiraan_pendapatan');
+        $perkiraan=\App\Perkiraan::get();
+        $tPerkiraan=[];
+        $bPerkiraan=[];
+        foreach($perkiraan as $p){
+            $tPerkiraan[]=$p->perkiraan_Pendapatan;
+            $bPerkiraan[]=$p->bulan->name_bulan;
+        }
+
+        return view('perkiraan_pendapatan.perkiraan_pendapatan',['tPerkiraan'=>$tPerkiraan,'bPerkiraan'=>$bPerkiraan]);
+    }
+
+    public function create(request $request)
+    {
+        $this->validate($request,[
+            'kode'=> 'required|unique:perkiraans,kode'
+        ]);
+        $perkiraan = new \App\Perkiraan;
+        $perkiraan->kode=$request->kode;
+        $perkiraan->bulan_id=$request->bulan;
+        $perkiraan->perkiraan_Pendapatan=$request->totalPerkiraan;
+        $perkiraan->tahun=$request->tahun;
+        $perkiraan->save();
+		 Alert::success('Sukses','Data Berhasil Disimpan');
+        // dd($perkiraan);
+        return redirect()->back();
     }
 }
