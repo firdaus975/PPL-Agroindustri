787d62a93c9a66e07a6cf35b0b3c25babe265829
firdaus975 <choirulahamad31@gmail.com>
2020-12-22 16:33:55 +0700

    Update sidebar.blade.php

--- resources/views/master/sidebar.blade.php
+++ resources/views/master/sidebar.blade.php
@@ -16,7 +16,16 @@
           </li>
           <li class=""><a class="nav-link" href="{{Route('perkiraan')}}"><i class="fas fa-chart-bar"></i> <span>Perkiraan Pendapatan</span></a></li>
           <li class="nav-item dropdown">
-            <a href="{{Route('keuangan')}}" class="nav-link"><i class="fas fa-money-check-alt"></i> <span>Keuangan</span></a>
+            <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-money-check-alt"></i> <span>Keuangan</span></a>
+			<ul class="dropdown-menu">
+			@if(Auth::user()->role_id == 1)
+                  <li><a class="nav-link" href="{{Route('pengeluaranPabrik')}}">Pengeluaran Pabrik</a></li>
+			  @endif
+				  @if(Auth::user()->role_id != 3)
+                  <li><a class="nav-link" href="{{Route('pendapatanPabrik')}}">Penjualan Karet</a></li>
+				@endif
+                  <li><a class="nav-link" href="{{Route('gaji',[Auth::user()->id,Auth::user()->role_id])}}">Gaji</a></li>
+                </ul>
           </li>
           @if(Auth::user()->role_id != 3)
           <li class="nav-item dropdown">
