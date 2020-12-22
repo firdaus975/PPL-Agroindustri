dcd90e16a50354d0d14681956f7381d895621dad
firdaus975 <choirulahamad31@gmail.com>
2020-12-22 16:33:11 +0700

    Update keuangan.blade.php

--- resources/views/keuangan/keuangan.blade.php
+++ resources/views/keuangan/keuangan.blade.php
@@ -5,3 +5,39 @@
 Keuangan
 
 @stop
+
+@section('content')
+
+<section>
+    <div class="section">
+        <div class="card">
+            <div class="card-header">
+              Featured
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">Pengeluaran Pabrik</h5>
+              <a href="{{Route('pengeluaranPabrik')}}" class="btn btn-primary">Lanjutkan</a>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-header">
+              Featured
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">Penjualan Pabrik</h5>
+              <a href="{{Route('pendapatanPabrik')}}" class="btn btn-primary">Lanjutkan</a>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-header">
+              Featured
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">Gaji Pegawai</h5>
+              <a href="{{Route('gaji')}}" class="btn btn-primary">Lanjutkan</a>
+            </div>
+          </div>
+    </div>
+</section>
+
+@stop
