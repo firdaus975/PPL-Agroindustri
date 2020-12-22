d2d27541e76ee40e9a20582c0d12ba18b7ec0d1d
firdaus975 <choirulahamad31@gmail.com>
2020-12-22 16:31:34 +0700

    Update KeuanganController.php

--- app/Http/Controllers/KeuanganController.php
+++ app/Http/Controllers/KeuanganController.php
@@ -10,4 +10,31 @@ class KeuanganController extends Controller
     {
         return view('keuangan.keuangan');
     }
+	
+	    public function pendapatan()
+    {
+        $penjualan=\App\PendapatanKaret::get();
+        return view('keuangan.pendapatan',['penjualan'=>$penjualan]);
+    }
+	
+	
+	    public function pengeluaran()
+    {
+        $pengeluaran=\App\Pengeluaran::get();
+        return view('keuangan.pengeluaran',['pengeluaran'=>$pengeluaran]);
+    }
+
+    public function gaji($id,$role)
+    {
+		if($role==1)
+		{
+			 return view('keuangan.gaji');
+		}else{
+		$gaji=\App\GajiPegawai::where('user_id',$id)->get();
+		return view('keuangan.rincian_gaji',['gaji'=>$gaji]);
+		}
+
+
+    }
+    
 }
